6aea1a9dcadd30c67170d827a88a220d29fd7ae4
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-07-31 11:10:39 +0200

    fix

--- src/components/menu-card.tsx
+++ src/components/menu-card.tsx
@@ -1,8 +1,7 @@
-import { HeartIcon, SoupIcon, UtensilsIcon } from "lucide-react";
+import { SoupIcon, UtensilsIcon } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Badge } from "./ui/badge";
 import { Separator } from "./ui/separator";
-import { Toggle } from "./ui/toggle";
 import { Favorite } from "./favorite";
 import { toggleFavorite } from "@/actions/toggle-favorite";
 
