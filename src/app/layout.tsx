e3761e3af0f58abe33fdbbd4a42156b72a09615c
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-07-31 11:09:42 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/next";
+// import { Analytics } from "@vercel/analytics/next";
 import localFont from "next/font/local";
 import "./globals.css";
 
